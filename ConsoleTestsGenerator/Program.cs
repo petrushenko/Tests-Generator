019567e6ef948cebe6ee236961461c58c3aa3924
petrushenko <igor.petrushenko1@gmail.com>
2019-11-27 18:10:23 +0300

    Added new console app for demo

--- ConsoleTestsGenerator/Program.cs
+++ ConsoleTestsGenerator/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using TestsGenerator;
 
 namespace ConsoleTestsGenerator
@@ -9,13 +12,28 @@ namespace ConsoleTestsGenerator
         private const string PathToSave = @"..\..\Tests\";
         public static void Main()
         {
-            Console.WriteLine("Threads count in start: " + Process.GetCurrentProcess().Threads.Count);
-            var generator = new NUnitTestsGenerator();
-            var waiter = generator.GenerateTests(new[] {@"..\..\Program.cs"}, PathToSave, 1, 1, 1);
-            waiter.Wait();
-            Console.WriteLine("Threads count after finish: " + Process.GetCurrentProcess().Threads.Count);
-            Console.WriteLine("Finished. Press any key to exit.");
-            Console.ReadKey();
+            Task waiter = null;
+            try
+            {
+                Console.WriteLine("Write list of test separating them by spaces");
+                var testFiles = Console.ReadLine().Split(' ').ToList();
+                Console.WriteLine("Write max amount of streams for reading");
+                var maxInputStreams = int.Parse(Console.ReadLine());
+                Console.WriteLine("Write max amount of streams for writing");
+                var maxOutStreams = int.Parse(Console.ReadLine());
+                Console.WriteLine("Write max amount of streams for generating tests");
+                var maxMainStreams = int.Parse(Console.ReadLine());
+                waiter =  new NUnitTestsGenerator().GenerateTests(testFiles, PathToSave, maxInputStreams, maxOutStreams,
+                    maxMainStreams);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            waiter?.Wait();
+            Console.WriteLine("Finished, check tests in [{0}] folder", Path.GetFullPath(PathToSave));
+            Console.ReadLine();
         }
     }
 }
